9e9fd9c7e7d0922c6a416994a69ae5dca0ed3251
Grégoire Unbekandt <gregoire.unbekandt@orange.com>
2018-02-18 23:03:32 +0100

    add classifiers

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst', 'r', 'utf-8') as f:
     readme = f.read()
 
 setup(name="paclair",
-      version="1.0.0",
+      version="1.0.1",
       description="Push and Analyse containers with Clair",
       long_description=readme,
       author="Grégoire UNBEKANDT",
@@ -21,12 +21,21 @@ setup(name="paclair",
       command_options={
                'build_sphinx': {
                    'project': ('setup.py', "paclair"),
-                   'version': ('setup.py', "1.0.0"),
-                   'release': ('setup.py', "1.0.0"),
+                   'version': ('setup.py', "1.0.1"),
+                   'release': ('setup.py', "1.0.1"),
                    'build_dir': ('setup.py', 'doc/sphinx/_build/')}},
      entry_points={
          'console_scripts': [
              'paclair = paclair.__main__:main'
          ]
      },
-     )
+     classifiers=[
+         "Programming Language :: Python",
+         "Programming Language :: Python :: 3",
+         "Programming Language :: Python :: 3.2",
+         "Programming Language :: Python :: 3.3",
+         "Programming Language :: Python :: 3.4",
+         "Programming Language :: Python :: 3.5",
+         "Programming Language :: Python :: 3.6",
+     ],
+)
