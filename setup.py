00e57cdf3940247d29afdd46ec6e444b97958e8c
Grégoire Unbekandt <gregoire.unbekandt@orange.com>
2018-04-04 16:18:31 +0200

    version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst', 'r', 'utf-8') as f:
     readme = f.read()
 
 setup(name="paclair",
-      version="1.0.1",
+      version="2.0.0",
       description="Push and Analyse containers with Clair",
       long_description=readme,
       author="Grégoire UNBEKANDT",
@@ -21,8 +21,8 @@ setup(name="paclair",
       command_options={
                'build_sphinx': {
                    'project': ('setup.py', "paclair"),
-                   'version': ('setup.py', "1.0.1"),
-                   'release': ('setup.py', "1.0.1"),
+                   'version': ('setup.py', "2.0.0"),
+                   'release': ('setup.py', "2.0.0"),
                    'build_dir': ('setup.py', 'doc/sphinx/_build/')}},
      entry_points={
          'console_scripts': [
